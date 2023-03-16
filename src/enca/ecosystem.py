2c1dc2ab1ab2fb7293176cb805628bf2398528e9
Thomas Danckaert <thomas.danckaert@vito.be>
2023-03-16 12:24:34 +0100

    Remove duplicates of 'framework' modules.

--- src/enca/ecosystem.py
+++
@@ -1,5 +0,0 @@
-ECOTYPE = 'ecosystem_type'
-ECO_ID = 'ecosystem_id'
-LANDCOVER_ID = 'landcover_id'
-# SHAPE_ID: integer identifier used when rasterizing vector data
-SHAPE_ID = 'SHAPE_ID'
--- src/enca/errors.py
+++
@@ -1,9 +0,0 @@
-class Error(Exception):
-    """Base class for exceptions.
-
-    Raise :obj:`Error` (or a subclass) for known errors where we can provide a meaningful error message.
-
-    :param message: Error message for the user."""
-
-    def __init__(self, message):
-        self.message = message  #: Error message for users.
