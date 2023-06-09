03eca2aebd7146f10ef070a88c87f8ac1d74bba5
Thomas Danckaert <thomas.danckaert@vito.be>
2023-06-09 10:35:29 +0200

    enca_plugin: Raise minimum sys4enca version to 0.4.1.

--- enca_plugin/install_deps.py
+++ enca_plugin/install_deps.py
@@ -17,8 +17,8 @@ from .pip_install_dialog import PipInstallDialog
 from .marvin_qgis_tools import osgeo4w
 
 _package_dist_name = 'sys4enca'  # Python package with core functionality
-_min_version = '0.3.1'  # Minimum required package version for the plugin.
-_version_next = '0.4.0'  # Next package version which may no longer be compatible with this version of the plugin.
+_min_version = '0.4.1'  # Minimum required package version for the plugin.
+_version_next = '0.5.0'  # Next package version which may no longer be compatible with this version of the plugin.
 _repo_url = 'https://artifactory.vgt.vito.be/api/pypi/python-packages/simple'
 
 
