156b8abcc56e480599ed35de585322e1efddfb4d
Lorenz Hambsch <lorenz.hambsch@vito.be>
2024-02-01 11:11:16 +0100

    update artifactory url change

--- enca_plugin/install_deps.py
+++ enca_plugin/install_deps.py
@@ -21,7 +21,7 @@ from .marvin_qgis_tools import osgeo4w
 _package_dist_name = 'sys4enca'  # Python package with core functionality
 _min_version = '0.9.2'  # Minimum required package version for the plugin.
 _version_next = '1.0.0'  # Next package version which may no longer be compatible with this version of the plugin.
-_repo_url = 'https://artifactory.vgt.vito.be/api/pypi/python-packages/simple'
+_repo_url = 'https://artifactory.vgt.vito.be/artifactory/api/pypi/python-packages/simple'
 
 
 def get_python_interpreter():
