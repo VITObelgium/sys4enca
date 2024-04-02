339bcecdc106d5c991a234ec00aba794cfe8748c
Robin Houdmeyers <robin.houdmeyers@vito.be>
2024-04-02 10:10:25 +0200

    Tag version 0.9.6

--- enca_plugin/install_deps.py
+++ enca_plugin/install_deps.py
@@ -23,7 +23,7 @@ from .marvin_qgis_tools import osgeo4w
 from .pip_install_dialog import PipInstallDialog
 
 _package_dist_name = 'sys4enca'  # Python package with core functionality
-_min_version = '0.9.6.dev4'  # Minimum required package version for the plugin.
+_min_version = '0.9.6'  # Minimum required package version for the plugin.
 _version_next = '1.0.0'  # Next package version which may no longer be compatible with this version of the plugin.
 _repo_url = 'https://artifactory.vgt.vito.be/artifactory/api/pypi/python-packages/simple'
 
